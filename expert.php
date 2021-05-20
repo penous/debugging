e514c101c20072a83d57767010f7361247bc4986
Tom Dirinck <tom.dirinck@gmail.com>
2021-05-20 16:47:58 +0200

    Cleaner.

--- expert.php
+++ expert.php
@@ -175,8 +175,8 @@ new_exercise(10);
 $areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
 $validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
 //from here on you can change the code
-$count = count($areTheseFruits)-1;
-for ($i=0; $i <= $count; $i++) {
+$count = count($areTheseFruits);
+for ($i=0; $i < $count; $i++) {
     if (!in_array($areTheseFruits[$i], $validFruits)) {
         unset($areTheseFruits[$i]);
     }
