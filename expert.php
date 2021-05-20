34c981a40da43e8a060339d0b7d06fb656d8bb26
Tom Dirinck <tom.dirinck@gmail.com>
2021-05-20 10:34:43 +0200

    Add boilerplate files.

---
+++ expert.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+
+function new_exercise($x)
+{
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[1];
+
+echo $monday;
